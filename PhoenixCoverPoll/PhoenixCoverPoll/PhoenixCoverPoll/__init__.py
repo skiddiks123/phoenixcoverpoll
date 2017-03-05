b3fdca50037f5050055718f951c9f7740b8e2549
Shatoidil <shatoidil@gmail.com>
2017-03-05 16:34:59 +0300

    02 Database connect

--- PhoenixCoverPoll/PhoenixCoverPoll/PhoenixCoverPoll/__init__.py
+++ PhoenixCoverPoll/PhoenixCoverPoll/PhoenixCoverPoll/__init__.py
@@ -9,13 +9,13 @@ app.secret_key = 'development key'
 app.config["MAIL_SERVER"] = "smtp.yandex.com"
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_SSL"] = True
-app.config["MAIL_USERNAME"] = 'pcp@fenixrostov.ru'
-app.config["MAIL_PASSWORD"] = '99Rostov99'
+app.config["MAIL_USERNAME"] = 'test@fenixrostov.ru'
+app.config["MAIL_PASSWORD"] = 'test'
 
 from PhoenixCoverPoll.views import mail
 mail.init_app(app)
  
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:32167@portal/pcp_base'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://rootroot:12345@portal/pcp_base'
 
 db.init_app(app)
 
